cdbc5fc6bf27975a8121c683b36527f9c83abf5f
Sylvia <sylviaprabudy@gmail.com>
2020-07-25 18:13:31 -0500

    finish writing methods and test, all pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,25 @@
-const Engineer = require('../lib/Employee');
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    const engineer = new Engineer('Jose');
+    const engineer = new Engineer('Howard');
+});
 
+test('set github account with constructor', () => {
+    const testValue = 'GitHubAccount';
+    const e = new Engineer('Foo', 1, 'engineer@email.com', testValue);
+    expect(e.github).toBe(testValue);
+})
+
+test('get github account with getGithub() method', () => {
+    const testValue = 'GitHubAccount';
+    const e = new Engineer('Foo', 1, 'engineer@email.com', testValue);
+    expect(e.getGitHub()).toBe(testValue);
+});
+
+
+test('getRole() return Engineer', () => {
+    const testValue = 'Engineer';
+    const e = new Engineer('Foo', 1, 'engineer@email.com', 'GitHubAccount');
+    expect(e.getRole()).toBe(testValue);
 });
